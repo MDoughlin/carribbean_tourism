5341ed513da6eb98f32b5193496e7844beb8e5b5
Micheline <MDoughlin@hotmail.com>
2024-11-14 20:11:42 -0500

    Destination Result Updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
-import { useState } from 'react';
-import  SearchBar from "./components/SearchBar"
-import DestinationResults from './components/DestinationResults';
+import "./App.css";
+import { useState } from "react";
+import SearchBar from "./components/SearchBar";
+import DestinationResults from "./components/DestinationResults";
 
 function App() {
-
-  const [results, setResults] = useState([])
+  const [results, setResults] = useState([]);
 
   return (
-    <div className='app'>
-  <h1>Which Paradise are you looking for?</h1>
-  <div className='search-bar-container'>
-  <SearchBar setResults={setResults}/>
-  <DestinationResults results={results} />
-  </div>
-  </div>
+    <div className="app">
+      <h1 className="page-title">Which Paradise are you looking for?</h1>
+      <div className="search-bar-container">
+        <SearchBar setResults={setResults} />
+        <DestinationResults results={results} />
+      </div>
+    </div>
   );
 }
 
--- src/components/DestinationResults.jsx
+++ src/components/DestinationResults.jsx
@@ -1,14 +1,47 @@
 import React from "react";
+import "./DestinationResults.css";
 
 const DestinationResults = ({ results }) => {
   return (
-    <div className="results-list">
+    <div>
       {results.map((country, index) => (
-        <div key={index}>
-          <h1>Country: {country.name.common}</h1>
-          <h1>Population: {country.population}</h1>
-          <h1>Language Spoken:</h1>
-        </div>
+        <>
+          <div className="information-container">
+            <div key={index}>
+              <p className="country-name">
+                <strong>Country:</strong> {country.name.common}
+              </p>
+              <p>
+                <strong>Capital:</strong> {country.capital}
+              </p>
+              <p>
+                <strong>Population:</strong>{" "}
+                {country.population.toLocaleString()}
+              </p>
+              <p>
+                <strong>Language Spoken:</strong>{" "}
+                {Object.values(country.languages).join(", ")}
+              </p>
+              <p>
+                <strong>Currencies: </strong>
+                {Object.values(country.currencies)
+                  .map(
+                    (currency) =>
+                      `${currency.name} (${currency.symbol || "N/A"})`
+                  )
+                  .join(", ")}
+              </p>
+              <p>
+                <strong>Country Map: </strong>
+                {country.maps.googleMaps}
+              </p>
+            </div>
+          </div>
+          <div className="flag-container">
+            <h1>Flag</h1>
+            {country.flag}
+          </div>
+        </>
       ))}
     </div>
   );
